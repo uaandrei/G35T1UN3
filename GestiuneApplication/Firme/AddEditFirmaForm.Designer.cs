b8bf2bb654a0805fa30b4bea9366ba7dd024e9fb
uaandrei <ua.andrei@gmail.com>
2012-06-18 16:15:04 +0300

    am setat AcceptButton = buton de salvare;

--- GestiuneApplication/Firme/AddEditFirmaForm.Designer.cs
+++ GestiuneApplication/Firme/AddEditFirmaForm.Designer.cs
@@ -232,6 +232,7 @@
             // 
             // AddEditFirmaForm
             // 
+            this.AcceptButton = this.saveBtn;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(337, 244);
--- GestiuneApplication/Produse/AddEditProdusForm.Designer.cs
+++ GestiuneApplication/Produse/AddEditProdusForm.Designer.cs
@@ -149,6 +149,7 @@
             // 
             // AddEditProdusForm
             // 
+            this.AcceptButton = this.saveTbox;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(228, 160);
